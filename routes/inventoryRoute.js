2b6a8d9d39c2325d1d8b9275613172de7f5318ca
Ashley Smith <ashesmsmith@gmail.com>
2024-09-26 11:14:51 -0600

    week 2 learning activity 1 part 3

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,9 @@
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const invController = require("../controlers/invController")
+
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index Route
 app.get("/", baseController.buildHome)
-// app.get("/", function(reg, res){
-//   res.render("index", {title: "Home"})
-// })
+// Inventory Routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
