b16894640ae62ddf8b8c2b97ba0498f69c3b9ce9
Ashley Smith <ashesmsmith@gmail.com>
2024-10-19 20:19:00 -0600

    email check attempt

--- public/js/acct-update.js
+++ public/js/acct-update.js
@@ -1,9 +1,10 @@
 // Assignment 5 - Task 5
+const accountModel = require("../../models/account-model")
 const email = document.querySelector("#account_email")
 
-email.addEventListener("change", async function (email) {
-    const emailExists = await accountModel.checkExistingEmail(email)
+email.addEventListener("change", function () {
+    const emailExists = accountModel.checkExistingEmail(email)
     if (emailExists) {
-        throw new Error("Email already exists. Please use different email")
+        throw new Error("Email already exists.")
     }
 })
